f6432962496c99db0100325db9d613dab72d4bd8
watson050308 <watson050308@gmail.com>
2024-01-14 23:48:30 +0800

    insert test data

--- backend/db/original_data.sql
+++ backend/db/original_data.sql
@@ -1,15 +1,3 @@
-INSERT INTO ACCOUNT_MANAGE(User_ID,User_Mail,User_Password,User_Auth,User_Name,User_Phone,Tax_ID,Main_Principal,Main_Connector,Main_Phone,User_Addr,User_Brand,User_Note)
-VALUES
-("A0001","RO.guangfu13@gmail.com","abc27477566","1","松山光復店-直營店","0227473388","25622146","蔡林高","呂知穎","0912345678","光復南路13巷29號1樓","A","月結")
-;
-
-INSERT INTO USER_BRAND_MAPPING(User_Brand, User_Brand_CH)
-VALUES
- ("A", "紅橘子店家")
-,("B", "愛餡貓店家")
-,("Z", "others")
-;
-
 //2024-1-14 sprint1:insert test data in tables
 INSERT INTO PRODUCT_MANAGE(Cost_Price,Delivery_Time,On_Sale_Price,Product_Count,Product_Desc,Product_ID,Product_Name,Product_Price,Product_State,Product_Unit,Safety_Count)
 VALUES
@@ -37,10 +25,13 @@ VALUES
 ,("J","愛餡貓冷凍")
 ;
 
-INSERT INTO ACCOUNT_MANAGE(Product_Type,Product_Type_CH,On_Sale_Price,Product_Count,Product_Desc,Product_ID,Product_Name,Product_Price,Product_State,Product_Unit,Safety_Count)
+INSERT INTO ACCOUNT_MANAGE(User_ID,User_Mail,User_Password,User_Auth,User_Name,User_Phone,Tax_ID,Main_Principal,Main_Connector,Main_Phone,User_Addr,User_Brand,User_Note)
 VALUES
- ("165","不確定","","18","東穎-1藍8入:line@","A0001","沙拉A5-8入/藍","235","正常供應中","盒","40")
-
+ ("A0001","RO.guangfu13@gmail.com","abc27477566","1","松山光復店-直營店","0227473388","25622146","蔡林高","呂知穎","0912345678","光復南路13巷29號1樓","A","月結")
+,("B0001","test002@gmail.com","abctest002","1","愛餡貓店家-測試店","0227473002","00200202","新垣結衣","星野源","0911111111","光復南路test01巷test01號","B","日結")
+,("C0001","test003@gmail.com","abctest003","1","廠商1-測試店","0227473003","00300303","多拉A夢","大雄","0922222222","光復南路test02巷test02號","C","日結下貨收現")
+,("D0001","test004@gmail.com","abctest004","1","廠商2-測試店","0227473004","00400404","蠟筆小新","小白","0933333333","光復南路test04巷test04號","D","月結匯款")
+,("Z0001","test005@gmail.com","abctest005","1","others-測試店","0227473005","00500505","卡比","瓦豆魯迪","0944444444","光復南路test05巷test05號","Z","月結日結")
 ;
 
 INSERT INTO USER_BRAND_MAPPING(User_Brand,User_Brand_CH)
